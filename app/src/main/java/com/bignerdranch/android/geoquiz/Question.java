e3e0fb179bc0f228a41c90d0f619a95d45e1e87b
Gerardo Sandoval <gerardo_90@hotmail.com>
2015-09-02 23:09:06 -0500

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Gerardo on 02/09/2015.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    
+}
